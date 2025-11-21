eeb71706d35f1edfd2e50eab1eeda4d041b02104
alex <alex101226@outlook.com>
2025-11-21 15:47:54 +0800

    删掉一些敏感信息

--- app/controllers/cmp/image_controller.py
+++ app/controllers/cmp/image_controller.py
@@ -5,16 +5,3 @@ from fastapi import APIRouter, Depends
 
 router = APIRouter(prefix="/cloud/image", tags=["系统镜像"])
 
-@router.get("/list")
-def list_images():
-    """
-    测试接口：获取阿里云镜像列表
-    """
-    client = CloudClientFactory.create_client(
-        'aliyun',
-        'LTAI5tHPqqYXb7GMUXCFZCS2',
-        'b4bARGw719ETqsxJODySfskRWVzAEA',
-        'ecs.aliyuncs.com'
-    )
-    result = client.list_images('cn-qingdao')
-    return {"items": result}
